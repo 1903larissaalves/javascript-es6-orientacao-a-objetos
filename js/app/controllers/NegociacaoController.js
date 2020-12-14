984d0d5c19564880b3af4a255405a13190f92841
Larissa Alves <1903larissaalves@gmail.com>
2020-12-14 17:22:33 -0300

    Criado lista, organizacao codigo

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -7,20 +7,33 @@ class NegociacaoController{
         this._data = $("#data");
         this._quantidade = $("#quantidade");
         this._valor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event){
 
         event.preventDefault();
 
-        var negociacao = new Negociacao(
+      
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+        console.log(this._listaNegociacoes.getNegociacoes());
+        
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
             DataHelper.textoParaData(this._data.value),
             this._quantidade.value,
-            this._valor.value
-        );
+            this._valor.value);
+    }
 
-        console.log(negociacao);
-        
+    _limpaFormulario(){
+        this._data.value = '';
+        this._quantidade.value = 1;
+        this._valor.value = 0.0;
+
+        this._data.focus();
     }
 
 }
