4115b488384fcbfd4a4397e167aa4d92da5dde8d
Larissa Alves <1903larissaalves@gmail.com>
2020-12-14 13:57:41 -0300

    Iniando controller, deletado index.js

---
+++ js/app/controllers/NegociacaoController.js
@@ -0,0 +1,19 @@
+class NegociacaoController{
+
+    adiciona(event){
+
+        event.preventDefault();
+
+        let data = document.querySelector("#data");
+        let quantidade = document.querySelector("#quantidade");
+        let valor = document.querySelector("#valor");
+
+        console.log(data.value);
+        console.log(quantidade.value);
+        console.log(valor.value);
+
+        
+        
+    }
+
+}
