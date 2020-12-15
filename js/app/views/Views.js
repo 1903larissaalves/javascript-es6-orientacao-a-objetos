9e54bf631edbb502e7282fbaab5da60863b34578
Larissa Alves <1903larissaalves@gmail.com>
2020-12-15 11:31:20 -0300

    Inserido erro de excecao na classe pai

--- js/app/views/Views.js
+++ js/app/views/Views.js
@@ -4,6 +4,10 @@ class Views{
         this._elemento = elemento;
     }
 
+    _template(){
+        throw new Error("O método template tem que ser implementado nas classes filhos");
+    }
+
     update(model){
         this._elemento.innerHTML = this._template(model);
     }
