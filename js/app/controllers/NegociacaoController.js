5f1f98b8829577d8510abb656d18a256b4501821
Larissa Alves <1903larissaalves@gmail.com>
2020-12-15 11:19:56 -0300

    Inserido mensagem de para usuario depois que adiciona uma negociacao

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -7,10 +7,15 @@ class NegociacaoController{
         this._data = $("#data");
         this._quantidade = $("#quantidade");
         this._valor = $("#valor");
+
         this._listaNegociacoes = new ListaNegociacoes();
         this._negociacoesView = new NegociacaoView($("#negociacoesView"));
-
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($("#mensagemView"));
+        this._mensagemView.update(this._mensagem);
+
 
     }
 
@@ -20,9 +25,10 @@ class NegociacaoController{
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = 'Negociação adicionada com sucesso!';
+        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
-        
-        console.log(this._listaNegociacoes.getNegociacoes());
         
     }
 
