e7aa6b8e8a3bcd4799f050787d8108cde71acf47
Larissa Alves <1903larissaalves@gmail.com>
2020-12-17 08:51:13 -0300

    Criado classe factory e criacao de stores

---
+++ js/app/services/ConnectionFactory.js
@@ -0,0 +1,39 @@
+var stores = ['negociacoes'];
+var version = 4;
+var dbName = 'teste';
+
+class ConnectionFactory{
+
+    constructor(){
+        throw new Error("Não é possível criar instâncias de ConnectionFactory");
+    }
+
+    static getConnection(){
+        return new Promise((resolve, reject) =>{
+            let openRequest = window.indexedDB.open(dbName, version);
+
+            openRequest.onupgradeneeded = e =>{
+                ConnectionFactory._createStores(e.target.result);
+            };
+
+            openRequest.onsuccess = e =>{
+                resolve(e.target.result);
+            };
+
+            openRequest.onerror = e =>{
+                reject(e.target.error.name);
+            };
+
+        });
+    }
+
+    static _createStores(connection){
+        stores.forEach(store =>{
+            if (connection.objectStoreName.contains(store)) {
+                connection.deleteObjectStore(store);
+            }
+
+            connection.creteObjectStore(store, {autoIncrement: true});
+        });
+    }
+}
