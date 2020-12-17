466e0f3be470b569e2f78ba469fe252cd6bbec54
Larissa Alves <1903larissaalves@gmail.com>
2020-12-17 11:36:04 -0300

    Persistindo dados quando a pagina é carregada

--- js/app/dao/NegociacaoDao.js
+++ js/app/dao/NegociacaoDao.js
@@ -23,4 +23,36 @@ class NegociacaoDao{
                 };
         });
     }
+
+    listaTodos(){
+        return new Promise((resolve, reject) =>{
+            
+            let cursor = this._connection
+                .transaction([this._store],'readwrite')
+                .objectStore(this._store)
+                .openCursor();
+            
+            let negociacoes = [];
+        
+            cursor.onsuccess = e =>{
+                //ponteiro
+                let atual = e.target.result;
+
+                if(atual){
+                    let dado = atual.value;
+
+                    negociacoes.push(new Negociacao(dado._data, dado._quantidade, dado._valor));
+
+                    atual.continue();
+                }else{
+                    resolve(negociacoes);
+                }
+            };
+
+            cursor.onerror = e =>{
+                console.log('Error:' + e.target.error);
+                reject("Não foi possível carregar as negociacoes");
+            };
+        });
+    }
 }
