0decb6d257c2e255d1c42723ab41fdc1d933cc13
Larissa Alves <1903larissaalves@gmail.com>
2020-12-17 17:05:29 -0300

    Mudancas

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -11,12 +11,14 @@ class NegociacaoController{
         this._listaNegociacoes = new Bind(
             new ListaNegociacoes(),
             new NegociacaoView($("#negociacoesView")),
-            'adiciona', 'apagaNegociacoes');
+            'adiciona', 'apagaNegociacoes', 'ordena', 'inverteOrdem');
 
         this._mensagem = new Bind(
             new Mensagem(),
             new MensagemView($("#mensagemView")),
             'texto');
+
+        this._ordemAtual = '';
 
         ConnectionFactory
             .getConnection()
@@ -62,13 +64,22 @@ class NegociacaoController{
             .getNegociacoes()
             .then(negociacoes => 
                 negociacoes.filter(negociacao =>
-                    !this._listaNegociacoes.negociacoes.some(negociacaoExixtente => JSON.stringify(negociacao) == JSON.stringify(negociacaoExixtente)))
+                    !this._listaNegociacoes.negociacoes.some(negociacaoExistente => JSON.stringify(negociacao) == JSON.stringify(negociacaoExistente)))
             )
             .then(negociacoes => negociacoes.forEach(negociacao => {
                 this._listaNegociacoes.adiciona(negociacao);
                 this._mensagem.texto = 'Negociações do período importadas.'   
             }))
             .catch(erro => this._mensagem.texto = erro);   
+        
+           /* let service = new NegociacaoService();
+            service
+                .getNegociacoes()
+                .then(negociacoes => negociacoes.forEach(negociacao => {
+                    this._listaNegociacoes.adiciona(negociacao);
+                    this._mensagem.texto = 'Negociações do período importadas'   
+                }))
+                .catch(erro => this._mensagem.texto = erro);    */
     }
 
     apaga(){
@@ -98,6 +109,16 @@ class NegociacaoController{
         this._valor.value = 0.0;
 
         this._data.focus();
+    }
+
+    ordena(coluna) {
+        
+        if(this._ordemAtual == coluna) {
+            this._listaNegociacoes.inverteOrdem(); 
+        } else {
+            this._listaNegociacoes.ordena((p, s) => p[coluna] - s[coluna]);    
+        }
+        this._ordemAtual = coluna;    
     }
 
 }
--- js/app/views/NegociacoesView.js
+++ js/app/views/NegociacoesView.js
@@ -11,10 +11,10 @@ class NegociacaoView extends Views{
         <table class="table table-hover table-bordered">
             <thead>
                 <tr>
-                    <th>DATA</th>
-                    <th>QUANTIDADE</th>
-                    <th>VALOR</th>
-                    <th>VOLUME</th>
+                <th onclick="negociacaoController.ordena('data')">DATA</th>
+                <th onclick="negociacaoController.ordena('quantidade')">QUANTIDADE</th>
+                <th onclick="negociacaoController.ordena('valor')">VALOR</th>
+                <th onclick="negociacaoController.ordena('volume')">VOLUME</th>
                 </tr>
             </thead>
             <tbody>
@@ -33,7 +33,7 @@ class NegociacaoView extends Views{
             <tfoot>
                 <td colspan="3"></td>
                 <td>
-                    ${ model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}
+                    ${model.volumeTotal}
                 </td>
             <tfoot>
      </table>
